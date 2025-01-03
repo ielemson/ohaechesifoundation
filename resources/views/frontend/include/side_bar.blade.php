6a1b0405d69e2801152703ba5029950551d361ad
Elemson Ifeanyi <ielemson@gmail.com>
2025-01-03 09:37:18 +0100

    updated side menu

--- resources/views/frontend/include/side_bar.blade.php
+++ resources/views/frontend/include/side_bar.blade.php
@@ -6,7 +6,7 @@
     </a>
     <div class="mobile-menu overflow-hidden"></div>
     <ul class="info pt-40">
-        <li><i class="fa-solid primary-color fa-location-dot"></i> <a href="mailto:{{ $setting->email }}">{{ $setting->email }}</a>
+        <li><i class="fa-solid primary-color fa-location-dot"></i> <a href="javascript:;">{{ $setting->address }}</a>
         </li>
         <li class="py-2"><i class="fa-solid primary-color fa-phone-volume"></i> <a
                 href="tel:{{ $setting->phone }}">{{ $setting->phone }}</a>
@@ -14,10 +14,11 @@
         <li><i class="fa-solid primary-color fa-paper-plane"></i> <a href="mailto:{{$setting->email}}">{{ $setting->email }}</a></li>
     </ul>
     <div class="social-icon mt-20">
-        <a href="#0"><i class="fa-brands fa-facebook-f"></i></a>
-        <a href="#0"><i class="fa-brands fa-twitter"></i></a>
-        <a href="#0"><i class="fa-brands fa-linkedin-in"></i></a>
-        <a href="#0"><i class="fa-brands fa-pinterest-p"></i></a>
+        <a href="{{ $setting->facebook }}"><i class="fa-brands fa-facebook-f"></i></a>
+        <a href="{{ $setting->twitter }}"><i class="fa-brands fa-twitter"></i></a>
+        <a href="{{ $setting->instagram }}"><i class="fa-brands fa-instagram"></i></a>
+        {{-- <a href="#0"><i class="fa-brands fa-pinterest-p"></i></a> --}}
+        
     </div>
     <button id="closeButton" class="text-white"><i class="fa-solid fa-xmark"></i></button>
 </div>
